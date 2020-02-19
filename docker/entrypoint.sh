c0fedb1bb5ee9c367b88596b77dbf33556500d0d
fosemberg <fosemberg@gmail.com>
2020-02-19 18:28:41 +0300

    delete docker, work in normal way

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#doing a faux-foreground-daemon
-while [ 1 ] ; do
-  node app.js
-  sleep 120 # every two minutes, eh? why not?
-done #There ain't no rest for the wicked, we are never done
--- src/env.settings.js
+++ src/env.settings.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-	environment: 'settings',
+	environment: 'trunk',
 	urls: [
 		{ name: 'Start', url: 'https://www.google.se/' },
 		{ name: 'Search', url: 'https://www.google.se/search?dcr=0&source=hp&q=lighthouse+influxdb+grafana&oq=lighthouse+influxdb+grafana' }
