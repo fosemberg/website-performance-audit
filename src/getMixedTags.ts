25f15aa9c5fd02bc02cc5d4d9adb6b238859eddb
fosemberg <fosemberg@gmail.com>
2020-02-26 18:05:07 +0300

    clean code

--- src/getMixedTags.ts
+++ src/getMixedTags.ts
@@ -11,22 +11,23 @@ export type MixedTags = Array<MixedTag>;
 export const getMixedTags = (tags: Array<Tag>): MixedTags => {
   const tagLengths = tags.map(tag => tag.values.length - 1);
   const allVariantsOfPoses = getAllVariantsOfPoses(tagLengths);
-  // console.log(allVariantsOfPoses);
 
   const mixedTags: MixedTags = [];
   for (const poses of allVariantsOfPoses) {
-    console.log(poses);
     const tagNames: Array<string> = [];
     let lighthouseFlags: LH.SharedFlagsSettings = {};
+
     for (const [i, pos] of poses.entries()) {
       const tag = tags[i].values[pos];
       tagNames.push(tag.name);
       lighthouseFlags = {...lighthouseFlags, ...tag.lighthouseFlags}
     }
+
     mixedTags.push({
       tagNames,
       lighthouseFlags,
     })
   }
-  return mixedTags
+
+  return mixedTags;
 };
