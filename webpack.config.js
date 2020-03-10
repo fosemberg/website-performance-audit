69192317c9d79696dbb71b739338b6ff92c5f730
fosemberg <fosemberg@gmail.com>
2020-03-10 13:17:48 +0300

    working webpack for front build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/front/index.js',
+  entry: './src/front/index.ts',
+  // devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'web'),
   },
 };
