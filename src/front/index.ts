c34e330baf2c101f266ed6547be6bfd18493b592
fosemberg <fosemberg@gmail.com>
2020-03-11 12:22:13 +0300

    es5 => es6

--- src/front/index.ts
+++ src/front/index.ts
@@ -2,22 +2,22 @@ import {createInputWithOptions} from "./createInputWithOptions";
 import {env} from "../../config/env";
 
 const environmentNames = env.environments.map(environment => environment.name);
-const siteNames = env.environments.reduce(
-  (environmentNames: string[], environment) =>
-    environmentNames.concat(
-      environment.sites.reduce(
-        (siteNames: string[], site) =>
-          environmentNames.includes(site.name)
-            ? siteNames
-            : siteNames.concat(site.name)
-        , []
-        )
-    ), []
-);
 
-createInputWithOptions(document.getElementById('env'), environmentNames, environmentNames[0]);
-createInputWithOptions(document.getElementById('site'), siteNames, siteNames[0]);
-createInputWithOptions(document.getElementById('site'), siteNames, siteNames[1]);
+const getSiteNamesByEnvironmentName = (environmentName) => {
+  for (const environment of env.environments) {
+    if (environmentName === environment.name) {
+      return environment.sites.map(site => site.name);
+    }
+  }
+};
+
+const onPickNewEnvironment = (newEnvironment) => {
+  const siteNames = getSiteNamesByEnvironmentName(newEnvironment);
+  createInputWithOptions(document.getElementById('site'), siteNames, siteNames && siteNames[0] || '');
+};
+
+createInputWithOptions(document.getElementById('env'), environmentNames, environmentNames[0], onPickNewEnvironment);
+onPickNewEnvironment(environmentNames[0]);
 
 const formElem = document.getElementById('formElem');
 if (formElem) {
